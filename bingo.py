69c26762b5c86ee8df8b718f8c5146e9b7ab6eaa
Kasper Biegun <kasper.biegun95@gmail.com>
2021-02-10 19:29:21 +0100

    Small refactor of bingo.py

--- bingo.py
+++ bingo.py
@@ -1,37 +1,46 @@
-from gtts import gTTS
-import subprocess
-import os
 import random
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
 
-
-DEVNULL = open(os.devnull, "wb")
 
 language = "en"
 letters = "BINGO"
-bingo_list = []
 used = []
 
-for n, letter in enumerate(letters):
-    for i in range(1, 16):
-        bingo_list.append(letter + " " + str(n * 15 + i))
 
-print(bingo_list)
+def prepare_bingo_list():
+    bingo_list = []
+    for n, letter in enumerate(letters):
+        for i in range(1, 16):
+            bingo_list.append(letter + " " + str(n * 15 + i))
 
-speech = gTTS(
-    text="Witajcie na zawodach w Bingo. Zaczynamy za trzy! dwa! jeden! START!",
-    lang=language,
-    slow=False,
-)
-speech.save("text.mp3")
-subprocess.run("play text.mp3 -t alsa", shell=True)
+    return bingo_list
+
+
+def play_welcome_message(language):
+    if language == 'pl':
+        welcome_text = "Witajcie na zawodach w Bingo. Zaczynamy za trzy! dwa! jeden! START!"
+    else:
+        welcome_text = "Welcome to the Bingo game. Start in three! two! one! Go!"
+    message = gTTS(text=welcome_text, lang=language, slow=False)
+    message.save("welcome_message.mp3")
+    sound = AudioSegment.from_file("welcome_message.mp3")
+    play(sound)
+
+
+bingo_list = prepare_bingo_list()
+play_welcome_message(language)
+
 
 while bingo_list:
     n = random.randint(0, len(bingo_list))
     try:
         print(bingo_list[n])
-        speech = gTTS(text=";     " + bingo_list[n], lang=language, slow=False)
-        speech.save("text.mp3")
-        subprocess.run("play text.mp3 -t alsa", stdout=None, shell=True)
+        pooled_number = gTTS(text=f";     {bingo_list[n]}", lang=language, slow=False)
+        pooled_number.save("pooled_number.mp3")
+        sound = AudioSegment.from_file("pooled_number.mp3")
+        play(sound)
         used.append(bingo_list[n])
         del bingo_list[n]
     except IndexError:
@@ -39,11 +48,8 @@ while bingo_list:
 
     except KeyboardInterrupt:
         used.sort()
-        # print(used)
         for letter in letters:
             print(
-                letter
-                + ": "
-                + ", ".join([element.split()[1] for element in used if letter in element])
+                f"{letter}: {', '.join([element.split()[1] for element in used if letter in element])}"
             )
         break
